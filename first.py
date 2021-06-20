16850aea671dd3c2d4e60cd7169918b3db61300f
alekszaf <a.zaforemska2@newcastle.as.uk>
2021-06-20 21:05:02 +0200

    Read from device

--- first.py
+++ first.py
@@ -1,1 +1,8 @@
-print("Hello")
+import serial
+
+device = serial.Serial('COM9', baudrate=115200)
+
+device.write(b'IDN\n')
+answer = device.readline()
+print(f'The answer is: {answer}')
+device.close()
